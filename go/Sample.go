b324d4e3e4a74687e390205bcff4e7ea26977e70
Aakash <aakash@MacBook-Pro-44.local>
2022-03-03 23:08:27 -0800

    reformating code

--- go/Sample.go
+++ go/Sample.go
@@ -34,13 +34,11 @@ func iterate(path string, skills_data string)  {
 		abs_file_path := path
 		
 		if s.HasSuffix(abs_file_path, ".pdf") {
-			// fmt.Println(abs_file_name)
 			pdf_data, err := readPdf(abs_file_path)
 			if err != nil {
 				fmt.Println("error while reading pdf")
 			}
 			skills_data_list := s.Split(skills_data, "\n")
-			// fmt.Println("Skills data ", skills_data_list)
 			fmt.Println("Length of Skills data ", len(skills_data_list))
 			fmt.Println("First skill ", skills_data_list[0])
 			skills_of_this_resume := make([]string, 0)
@@ -55,29 +53,9 @@ func iterate(path string, skills_data string)  {
 		return nil
 	})
 }
-// func readAndTagPdf(path string, skills_data string) {
-// 	// fmt.Println(path)
-// 	res, err := docconv.ConvertPath(path)
-//     if err != nil {
-//         fmt.Println(err)
-//     }
-// 	fmt.Println(res)
-// 	//iterate over skills and check if skill exists in res
-// 	skills_data_list := s.Split(skills_data, "\n")
-// 	// fmt.Println("Skills data ", skills_data_list)
-// 	fmt.Println("Length of Skills data ", len(skills_data_list))
-// 	fmt.Println("First skill ", skills_data_list[0])
-// 	skills_of_this_resume := make([]string, 0)
-// 	for i := 0; i < len(skills_data_list); i++ {
-// 		if s.Contains(res,skills_data_list[i]) {
-
-// 		}
-// 	}
-// }
 
 func readPdf(path string) (string, error) {
 	f, r, err := pdf.Open(path)
-	// remember close file
     defer f.Close()
 	if err != nil {
 		return "", err
